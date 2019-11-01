6bbe1bf67ac4755c6515d516430042713761b8f1
julyicecream2001 <56427814+julyicecream2001@users.noreply.github.com>
2019-11-01 15:18:41 +0800

    Add files via upload

---
+++ wh/stof.c
@@ -0,0 +1,13 @@
+#include<studio.h>
+ int main()
+ {
+        int n=0;
+        int n1=0;
+        int n2=0;
+        scanf("%d",&n2);
+        while (n2!=8)
+        if(n1==1)
+        n=n+1
+ }
+print("%d\n",n);
+}
